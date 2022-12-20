575c0bae78afdf5f70a3c6ed3895f3ef0c0ce734
Egor) <kes2079531@gmail.com>
2022-12-20 06:05:17 +0300

    added feb

--- difficultConds/N5.py
+++ difficultConds/N5.py
@@ -3,10 +3,13 @@ n = int(input())
 if n > 12:
     print('Ошибка')
 else:
-    if n > 7:
+    if n == 2: print('28')
+
+    elif n > 7:
         if n % 2 == 0:
             print('31')
         else: print('30')
+
     else:
         if n % 2 == 0:
             print('30')
