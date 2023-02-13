f99a6a2b7169c1d4f70aede0f510580814adc322
Egor) <kes2079531@gmail.com>
2023-02-13 07:35:53 +0300

    added rec

---
+++ HWorks/09_02/friendNums.py
@@ -0,0 +1,14 @@
+def sumOfDevs(n):
+    sumOfDevs = 0
+
+    for i in range(2, n // 2 + 1):
+        if n % i == 0:
+            sumOfDevs += i
+
+    return sumOfDevs
+
+for j in range(1, 5000):
+    for k in range(1, 10000):
+        if sumOfDevs(j) == k and sumOfDevs(k) == j \
+                and k >= j:
+            print(j, k)
--- HWorks/for08_02_2023/fibInArr.py
+++ HWorks/for08_02_2023/fibInArr.py
@@ -2,10 +2,34 @@
 заполнить массив первыми числами Фибоначчи
 '''
 
+'''
+через цикл
+
 n = int(input())
 fib = [0, 1]
 
 for i in range(2, n + 1):
     fib.append(fib[i - 2] + fib[i - 1])
+
+print(fib)
+'''
+
+'''
+через рекурсию'''
+
+n = int(input())
+fib = []
+
+
+def f(n):
+    if n == 1 or n == 0:
+        return 1
+
+    else:
+        return f(n - 1) + f(n - 2)
+
+
+for i in range(n):
+    fib.append(f(i + 1))
 
 print(fib)
